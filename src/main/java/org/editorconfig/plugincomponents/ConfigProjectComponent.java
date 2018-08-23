0616d4ff27d6b92f0cefcd3242b0df4494059721
Valery Semenchuk <vistall.valeriy@gmail.com>
2018-08-23 03:03:12 +0300

    migrated to services

--- src/main/java/org/editorconfig/plugincomponents/ConfigProjectComponent.java
+++ src/main/java/org/editorconfig/plugincomponents/ConfigProjectComponent.java
@@ -1,69 +1,64 @@
 package org.editorconfig.plugincomponents;
 
-import com.intellij.AppTopics;
-import com.intellij.openapi.components.ProjectComponent;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.EditorFactory;
-import com.intellij.openapi.editor.ex.EditorEx;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFileAdapter;
-import com.intellij.openapi.vfs.VirtualFileEvent;
-import com.intellij.openapi.vfs.VirtualFileManager;
-import com.intellij.util.messages.MessageBus;
+import javax.inject.Singleton;
+
 import org.editorconfig.configmanagement.EditorSettingsManager;
 import org.editorconfig.configmanagement.EncodingManager;
 import org.editorconfig.configmanagement.LineEndingsManager;
 import org.jetbrains.annotations.NotNull;
+import com.intellij.AppTopics;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.EditorFactory;
+import com.intellij.openapi.editor.ex.EditorEx;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFileEvent;
+import com.intellij.openapi.vfs.VirtualFileListener;
+import com.intellij.openapi.vfs.VirtualFileManager;
+import com.intellij.util.messages.MessageBus;
 
-public class ConfigProjectComponent implements ProjectComponent {
+@Singleton
+public class ConfigProjectComponent
+{
+	public ConfigProjectComponent(final Project project, final EditorFactory editorFactory, VirtualFileManager virtualFileManager)
+	{
+		// Register project-level config managers
+		MessageBus bus = project.getMessageBus();
+		EditorSettingsManager editorSettingsManager = new EditorSettingsManager(project);
+		EncodingManager encodingManager = new EncodingManager(project);
+		LineEndingsManager lineEndingsManager = new LineEndingsManager(project);
+		bus.connect().subscribe(AppTopics.FILE_DOCUMENT_SYNC, encodingManager);
+		bus.connect().subscribe(AppTopics.FILE_DOCUMENT_SYNC, editorSettingsManager);
+		bus.connect().subscribe(AppTopics.FILE_DOCUMENT_SYNC, lineEndingsManager);
+		virtualFileManager.addVirtualFileListener(new VirtualFileListener()
+		{
+			@Override
+			public void fileCreated(@NotNull VirtualFileEvent event)
+			{
+				updateOpenEditors(event);
+			}
 
-  public ConfigProjectComponent(final Project project, final EditorFactory editorFactory) {
-    // Register project-level config managers
-    MessageBus bus = project.getMessageBus();
-    EditorSettingsManager editorSettingsManager = new EditorSettingsManager(project);
-    EncodingManager encodingManager = new EncodingManager(project);
-    LineEndingsManager lineEndingsManager = new LineEndingsManager(project);
-    bus.connect().subscribe(AppTopics.FILE_DOCUMENT_SYNC, encodingManager);
-    bus.connect().subscribe(AppTopics.FILE_DOCUMENT_SYNC, editorSettingsManager);
-    bus.connect().subscribe(AppTopics.FILE_DOCUMENT_SYNC, lineEndingsManager);
-    VirtualFileManager.getInstance().addVirtualFileListener(new VirtualFileAdapter() {
-      @Override
-      public void fileCreated(@NotNull VirtualFileEvent event) {
-        updateOpenEditors(event);
-      }
+			@Override
+			public void fileDeleted(@NotNull VirtualFileEvent event)
+			{
+				updateOpenEditors(event);
+			}
 
-      @Override
-      public void fileDeleted(@NotNull VirtualFileEvent event) {
-        updateOpenEditors(event);
-      }
+			@Override
+			public void contentsChanged(@NotNull VirtualFileEvent event)
+			{
+				updateOpenEditors(event);
+			}
 
-      @Override
-      public void contentsChanged(@NotNull VirtualFileEvent event) {
-        updateOpenEditors(event);
-      }
-
-      private void updateOpenEditors(VirtualFileEvent event) {
-        if (".editorconfig".equals(event.getFile().getName())) {
-          for (Editor editor : editorFactory.getAllEditors()) {
-            ((EditorEx)editor).reinitSettings();
-          }
-        }
-      }
-    }, project);
-  }
-
-  public void initComponent() {
-  }
-
-  public void disposeComponent() {
-  }
-
-  @NotNull
-  public String getComponentName() {
-    return "ConfigProjectComponent";
-  }
-
-  public void projectOpened() {}
-
-  public void projectClosed() {}
+			private void updateOpenEditors(VirtualFileEvent event)
+			{
+				if(".editorconfig".equals(event.getFile().getName()))
+				{
+					for(Editor editor : editorFactory.getAllEditors())
+					{
+						((EditorEx) editor).reinitSettings();
+					}
+				}
+			}
+		}, project);
+	}
 }
--- src/main/java/org/editorconfig/plugincomponents/SettingsProviderComponent.java
+++ src/main/java/org/editorconfig/plugincomponents/SettingsProviderComponent.java
@@ -1,51 +1,44 @@
 package org.editorconfig.plugincomponents;
 
-import com.intellij.openapi.components.ApplicationComponent;
-import com.intellij.openapi.components.ServiceManager;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.project.Project;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Singleton;
+
 import org.editorconfig.Utils;
 import org.editorconfig.core.EditorConfig;
 import org.editorconfig.core.EditorConfig.OutPair;
 import org.editorconfig.core.EditorConfigException;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.project.Project;
 
-import java.util.ArrayList;
-import java.util.List;
+@Singleton
+public class SettingsProviderComponent
+{
+	private EditorConfig editorConfig;
 
-public class SettingsProviderComponent implements ApplicationComponent {
-  private static final Logger LOG = Logger.getInstance("#org.editorconfig.plugincomponents.SettingsProviderComponent");
+	public SettingsProviderComponent()
+	{
+		editorConfig = new EditorConfig();
+	}
 
-  private EditorConfig editorConfig;
+	public static SettingsProviderComponent getInstance()
+	{
+		return ServiceManager.getService(SettingsProviderComponent.class);
+	}
 
-  public SettingsProviderComponent() {
-    editorConfig = new EditorConfig();
-  }
-
-  public static SettingsProviderComponent getInstance() {
-    return ServiceManager.getService(SettingsProviderComponent.class);
-  }
-
-  public List<OutPair> getOutPairs(Project project, String filePath) {
-    final List<OutPair> outPairs;
-    try {
-      outPairs = editorConfig.getProperties(filePath);
-      return outPairs;
-    }
-    catch (EditorConfigException error) {
-      Utils.invalidConfigMessage(project, error.getMessage(), "", filePath);
-      return new ArrayList<OutPair>();
-    }
-  }
-
-  public void initComponent() {
-  }
-
-  public void disposeComponent() {
-  }
-
-  @NotNull
-  public String getComponentName() {
-    return "SettingsProviderComponent";
-  }
+	public List<OutPair> getOutPairs(Project project, String filePath)
+	{
+		final List<OutPair> outPairs;
+		try
+		{
+			outPairs = editorConfig.getProperties(filePath);
+			return outPairs;
+		}
+		catch(EditorConfigException error)
+		{
+			Utils.invalidConfigMessage(project, error.getMessage(), "", filePath);
+			return new ArrayList<OutPair>();
+		}
+	}
 }
